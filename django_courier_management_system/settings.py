7fd95138212b35bac8934a912df9601f2b4ebb1f
Mahmud-Alam <official.mahmud23@gmail.com>
2021-10-27 01:26:59 +0600

    update settings.py

--- django_courier_management_system/settings.py
+++ django_courier_management_system/settings.py
@@ -124,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
@@ -132,8 +134,10 @@ STATICFILES_DIRS = [
     #os.path.join(BASE_DIR,'static')
 ]
 
-MEDIA_ROOT = BASE_DIR/'static/images'
-STATIC_ROOT = BASE_DIR/'staticfiles'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+#MEDIA_ROOT = BASE_DIR/'static/images'
+#STATIC_ROOT = BASE_DIR/'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
